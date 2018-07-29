8c5f73ebef3eac5841543b0430af01085f8ea3d4
yin <2224168716@qq.com>
2018-07-30 07:26:56 +0800

    add cart modelJs

---
+++ pages/cart/cart-model.js
@@ -0,0 +1,19 @@
+import { Base } from '../../utils/base.js';
+
+class Cart extends Base {
+  constructor(){
+    super();
+  }
+
+    /*
+    * 加入到购物车
+    * 如果之前没有样的商品，则直接添加一条新的记录， 数量为 counts
+    * 如果有，则只将相应数量 + counts
+    * @params:
+    * item - {obj} 商品对象,
+    * counts - {int} 商品数目,
+    * */
+  add(item,counts){
+
+  }
+}
