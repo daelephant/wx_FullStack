43b8dafbb377e699a10ba6a033b45f5b8460d646
yin <2224168716@qq.com>
2018-08-02 23:39:22 +0800

    add Order and address

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,20 +1,57 @@
 // pages/order/order.js
+//import { Order } from '../order/order-model.js';
+import { Cart } from '../cart/cart-model.js';
+import { Address } from '../../utils/address.js';
+
+//var order = new Order();
+var cart = new Cart();
+var address = new Address();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    fromCartFlag: true,
+    addressInfo: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var productsArr;
+    this.data.account = options.account;
+
+    productsArr = cart.getCartDataFromLocal(true);
+    this.setData({
+      productsArr:productsArr,
+      account:options.account,
+      orderStatus:0
+    });
   },
 
+  editAddress:function(event){
+    var that = this;
+    wx.chooseAddress({
+      success:function(res){
+        console.log(res);
+        var addressInfo = {
+          name:res.userName,
+          mobile:res.telNumber,
+          totalDetail: address.setAddressInfo(res)
+        };
+        that._bindAddressInfo(addressInfo);
+      }
+    })
+  },
+
+  /*绑定地址信息*/
+  _bindAddressInfo: function (addressInfo) {
+    this.setData({
+      addressInfo: addressInfo
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
